69b3923c7e7dbd79a5c46541644df2bf832b2fd4
Kvazar3S273 <ba2h007@gmail.com>
2021-07-16 00:19:28 +0300

    create products

---
+++ js/products.js
@@ -0,0 +1,7 @@
+﻿$(function () {
+    var $myModal = $("#myModal");
+    $("#btnAddProduct").on("click", function () {
+        //alert("Ви нажали кнопку");
+        $myModal.modal("show");
+    });
+});
